6c74812e7834642b45c2186c0c428f2d83ebea6d
Domenic SanGiovanni <domsangio@gmail.com>
2023-12-12 22:36:50 -0500

    Add back in memoi

--- src/day12/day12b.rs
+++ src/day12/day12b.rs
@@ -2,25 +2,11 @@ use std::collections::HashMap;
 
 use itertools::Itertools;
 
-fn print_info(idx: &usize, springs: &Vec<char>, lengths: &[i64]) {
-    print!("Idx: {}, ", idx);
-    for c in springs.iter().skip(*idx) {
-        print!("{}", c);
-    }
-    print!(", lengths: ");
-    for x in lengths.iter() {
-        print!("{}, ", x);
-    }
-    print!("\n");
-}
-
 fn calculate_line_num_ways(
     idx: &usize,
     springs: &Vec<char>,
     lengths: &[i64],
-    memoi: &mut HashMap<(usize, usize), i64>,
-    mut placed: Vec<(usize, usize)>,
-    og_lengths: &[i64]
+    memoi: &mut HashMap<(usize, usize), i64>
 ) -> i64 {
     // print_info(idx, springs, lengths);
 
@@ -29,6 +15,7 @@ fn calculate_line_num_ways(
     }
     if *idx >= springs.len() {
         if lengths.len() != 0 {
+            memoi.insert((idx.clone(), lengths.len()), 0);
             return 0;
         }
     }
@@ -37,66 +24,12 @@ fn calculate_line_num_ways(
         // make suire no more '#' in the list
         for i in (*idx)..springs.len() {
             if springs[i] == '#' {
+                memoi.insert((idx.clone(), lengths.len()), 0);
                return 0;
             }
         }
 
-        let mut replaced_string: Vec<char> = Vec::new();
-        for a in springs.iter() {
-            replaced_string.push(a.clone());
-        }
-        for (idx, length) in placed.iter() {
-            for i in *idx..(*idx + *length) {
-                replaced_string[i] = '#';
-            }
-        }
-        for i in 0..replaced_string.len() {
-            if replaced_string[i] == '?' {
-                replaced_string[i] = '.';
-            }
-        }
-
-        let pre_truncated_str = replaced_string.clone();
-
-        // print!("new string: ");
-        // for x in replaced_string.iter() {
-        //     print!("{}", x);
-        // }
-        // println!();
-
-        let mut i = 0;
-        while i < replaced_string.len() - 1 {
-            if replaced_string[i] == '.' && replaced_string[i + 1] == '.' {
-                replaced_string.remove(i);
-            } else {
-                i += 1;
-            }
-        }
-        if replaced_string[0] == '.' {
-            replaced_string.remove(0);
-        }
-        if replaced_string[replaced_string.len() - 1] == '.' {
-            replaced_string.remove(replaced_string.len() - 1);
-        }
-
-        // print!("replaced str: ");
-        // for x in replaced_string.iter() {
-        //     print!("{}", x);
-        // }
-        // println!();
-        
-        for (length_index, line) in replaced_string.split(|x| *x == '.').enumerate() {
-            if length_index >= og_lengths.len() || og_lengths[length_index] as usize != line.len() {
-                println!("ERROR!!!!");
-                print_info(&0, springs, og_lengths);
-                print!("new found string: ");
-                for x in pre_truncated_str.iter() {
-                    print!("{}", x);
-                }
-                println!();
-            }
-        }
-
+        memoi.insert((idx.clone(), lengths.len()), 1);
         return 1;
     }
 
@@ -113,22 +46,21 @@ fn calculate_line_num_ways(
                 in_a_row += 1;
             }
             if (in_a_row as i64) < match_length {
+                memoi.insert((idx.clone(), lengths.len()), 0);
                 return 0;
             } else if (i + match_length as usize) < springs.len() && springs[i + match_length as usize] == '#'{
+                memoi.insert((idx.clone(), lengths.len()), 0);
                 return 0;
             }
 
-            placed.push((i, match_length as usize));
             // make sure this skipping business works (pull first value off lengths too)
             let ret = calculate_line_num_ways(
                 &(i + match_length as usize + 1),
                 springs,
                 &lengths[1..],
-                memoi,
-                placed,
-                og_lengths
+                memoi
             );
-            // memoi.insert((idx.clone(), lengths.len()), ret);
+            memoi.insert((idx.clone(), lengths.len()), ret);
             return ret;
         }
         '?' => {
@@ -145,37 +77,26 @@ fn calculate_line_num_ways(
             }
             if (in_a_row as i64) >= lengths[0] {
                 if i + in_a_row >= springs.len() || springs[i + in_a_row] != '#' {
-                    // can the match go here?
-                    // it can only go here if yes length matches, and the next char is a '.' or a #
-
-                    // println!("Index: {} can fit the match of length: {} here", i, lengths[0]);
-                    // yes we can set it here
-                    // println!("Returning num_ways for two paths: idx: {}");
-                    // print_info(&(i + lengths[0] as usize + 1), springs, &lengths[1..]);
-                    // print_info(&(i + 1), springs, lengths);
-                    let c = placed.clone();
-                    placed.push((i, lengths[0] as usize));
-
                     let ret = calculate_line_num_ways(
                         &(i + lengths[0] as usize + 1),
                         springs,
                         &lengths[1..],
-                        memoi,
-                        placed,
-                        og_lengths
-                    ) + calculate_line_num_ways(&(i + 1), springs, lengths, memoi, c, og_lengths);
+                        memoi
+                    ) + calculate_line_num_ways(&(i + 1), springs, lengths, memoi);
 
-                    // memoi.insert((idx.clone(), lengths.len()), ret);
+                    memoi.insert((idx.clone(), lengths.len()), ret);
                     return ret;
                 }
             }
 
-            let ret = calculate_line_num_ways(&(i + 1), springs, lengths, memoi, placed, og_lengths);
-            // memoi.insert((idx.clone(), lengths.len()), ret);
+            let ret = calculate_line_num_ways(&(i + 1), springs, lengths, memoi);
+            memoi.insert((idx.clone(), lengths.len()), ret);
             return ret;
         }
         _ => {
-            return calculate_line_num_ways(&(idx + 1), springs, lengths, memoi, placed, og_lengths);
+            let ret = calculate_line_num_ways(&(idx + 1), springs, lengths, memoi);
+            memoi.insert((idx.clone(), lengths.len()), ret);
+            return ret;
         }
     }
 }
@@ -202,18 +123,11 @@ pub fn day12b(input: &Vec<&str>) {
             &0,
             &expanded_springs,
             &expanded_lengths[..],
-            &mut memoi,
-            Vec::<(usize, usize)>::new(),
-            &expanded_lengths[..]
+            &mut memoi
         ) as i64;
-        // if t  == 0 {
-        //     println!("0: {}", line);
-        // }
-        // println!("Line: {} calculated: {}", line, t);
+
         sum += t;
-        // for ((idx, lengths), calc) in memoi.iter() {
-        //     println!("idx: {} - length left: {}, calculated: {}", idx, lengths, calc);
-        // }
+
         
         if i % 10 == 0 {
             println!("Processed another 10: {}", i);
