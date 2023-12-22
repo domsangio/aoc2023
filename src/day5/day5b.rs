eacf0a42cbb5ba6850676885f1f2f5c65569afd1
domsangio <domenic_sangiovanni@yahoo.com>
2023-12-22 15:39:35 -0500

    Working through 5b

--- src/day5/day5b.rs
+++ src/day5/day5b.rs
@@ -1,11 +1,11 @@
-use itertools::{Itertools, min};
-use std::collections::{HashMap, VecDeque};
+use itertools::{min, Itertools};
 use regex::Regex;
+use std::collections::{HashMap, VecDeque};
 
 struct Conversion {
     dest: String,
     source: String,
-    func: Vec<(i64, i64, i64)>
+    func: Vec<(i64, i64, i64)>,
 }
 
 fn get_starting_seeds(line: &str) -> Vec<(i64, i64)> {
@@ -18,7 +18,7 @@ fn get_starting_seeds(line: &str) -> Vec<(i64, i64)> {
     ret
 }
 
-fn construct_conversions<'a>(lines: &Vec<&str>, conversions: &'a mut Vec<Conversion>, conversion_index_map: &'a mut HashMap<&'a str, usize>) {
+fn construct_conversions<'a>(lines: &Vec<&str>, conversions: &'a mut Vec<Conversion>) {
     let mut input_iter = lines.iter().skip(2);
     let conversion_pattern = Regex::new(r"^([a-z]+)-to-([a-z]+) map:$").unwrap();
 
@@ -36,86 +36,106 @@ fn construct_conversions<'a>(lines: &Vec<&str>, conversions: &'a mut Vec<Convers
 
             let captures = func_pattern.captures(&func_line).unwrap();
             let convert_tup = (1..=3)
-                .map(|index: usize| captures.get(index).unwrap().as_str().parse::<i64>().unwrap())
-                .collect_tuple::<(_, _, _)>().unwrap();
-            let insert_index =  match func.binary_search_by_key(&convert_tup.0, |(a, _, _)| *a) {
+                .map(|index: usize| {
+                    captures
+                        .get(index)
+                        .unwrap()
+                        .as_str()
+                        .parse::<i64>()
+                        .unwrap()
+                })
+                .collect_tuple::<(_, _, _)>()
+                .unwrap();
+            let insert_index = match func.binary_search_by_key(&convert_tup.0, |(a, _, _)| *a) {
                 Ok(i) => i,
-                Err(i) => i
+                Err(i) => i,
             };
             func.insert(insert_index, convert_tup);
         }
 
-        conversions.push(
-            Conversion {
-                dest: captures.get(1).unwrap().as_str().to_string().clone(),
-                source: captures.get(2).unwrap().as_str().to_string().clone(),
-                func: func
-            }
-        );
-
-        // conversion_index_map.insert(&conversions.last().unwrap().source, conversions.len() - 1);
-    }
-
-    for (i, c) in conversions.iter().enumerate() {
-        conversion_index_map.insert(&c.source, i);
+        conversions.push(Conversion {
+            dest: captures.get(1).unwrap().as_str().to_string().clone(),
+            source: captures.get(2).unwrap().as_str().to_string().clone(),
+            func: func,
+        });
     }
 }
 
-
-// takes in a range [LB, amount) and seed range [Start, offset] and returns start, amount
-// ex:: [10, 15), [12, 5] --> [12, 3]
-fn find_overlap(conversion_range: (i64, i64), seed_range: (i64, i64)) -> (i64, i64) {
-    let (conversion_lb, conversion_amount) = conversion_range;
-    let (seed_range_lb, seed_range_amount) = seed_range;
-
-    // perhaps off by one
-    if conversion_lb <= seed_range_lb && seed_range_lb < conversion_lb + conversion_amount {
-        return (seed_range_lb, std::cmp::min(seed_range_amount, (conversion_lb + conversion_amount) - (seed_range_lb + seed_range_amount)));
-    }
-
-    return (0, 0);
+fn is_overlap(
+    current_seed_start: i64,
+    range_start: i64,
+    range_length: i64,
+) -> bool {
+    current_seed_start >= range_start && current_seed_start < range_start + range_length
 }
 
-// left part of tuple is converted, right part is remaining
-fn convert_range(seed_range: (i64, i64), c: &Conversion) -> ((i64, i64), (i64, i64)) {
-    for conversion_func in c.func.iter() {
-        let (converted, amount) = find_overlap((conversion_func.0, conversion_func.2), seed_range);
-        if converted != 0 && amount != 0 {
-            // convert this subsection
-            let converted_tuple = (conversion_func.1 + (converted - seed_range.0), amount);
+// return ((converted_range, amount), (front_range, length), (back_range, length))
+fn get_overlap(
+    current_seed_start: i64,
+    current_seed_length: i64,
+    range_start: i64,
+    range_length: i64,
+) -> ((i64, i64), (i64, i64), (i64, i64)) {
+    // find start of current range and amount
+    let start = std::cmp::max(current_seed_start, range_start);
+    let end = std::cmp::min(range_start + range_length, start + current_seed_start);
 
-            // also is remaining tuple even correct idk
-            let remaining_tuple = (conversion_func.0 + conversion_func.2, seed_range.1 - amount);
-            return (converted_tuple, remaining_tuple);
-        }
-    }
+    let front_start = std::cmp::min(start, current_seed_start);
+    let front_length = start - front_start;
 
-    return (seed_range, (0,0));
+    let end_start = std::cmp::max(end, current_seed_start + current_seed_length);
+    let end_length = std::cmp::max(0, range_start + range_length - end_start); // TODO unnecessary max?
+
+    return (
+        (start, end - start),
+        (front_start, front_length),
+        (end_start, end_length),
+    );
 }
+
+// how do we convert a range? 
+// we have source dest range
+// dest + (seed start - source convert start)
 
 /// need to switch to a quewue like object
 pub fn day5b(input: &Vec<&str>) {
     // need to grab our starting seeds (sorted - mutable so we change them)
-    let mut starting_seeds: Vec<(i64, i64)> = get_starting_seeds(input[0]);
-    starting_seeds.sort();
-    // TODO since its sorted can be clever
+    let mut current_seeds: Vec<(i64, i64)> = get_starting_seeds(input[0]);
 
-    // list of conversions, map of string to conversion-index in vec
+    // list of conversions we iterate over this
     let mut conversions: Vec<Conversion> = Vec::new();
-    let mut conversion_index_map: HashMap<&str, usize> = HashMap::new();
+    construct_conversions(input, &mut conversions);
 
-    construct_conversions(input, &mut conversions, &mut conversion_index_map);
-    
     // for every conversion, try to convert this seed range
-    for c in conversions {
-        let mut processed: VecDeque<(i64, i64)> = VecDeque::new();
-        let mut unprocessed = VecDeque::from_iter(starting_seeds.iter());
+    for c in conversions.iter() {
+        let mut i: usize = 0;
+        while i < current_seeds.len() {
+            // convert this range in place, throw whats left at the end
+            // unefficiently, lets just find an overlap, convert it and add rest to end -- technically, we know that
+            // since we converted the front part of the range, it cant be this one and we should really search better lets see
+            let (curr_seed_start, curr_seed_range) = current_seeds[i];
+ 
+            // TODO binary search here
+            for (source, dest, amount) in c.func.iter() {
+                if is_overlap(curr_seed_start, *source, *amount) {
+                    let (converted, front, back) = get_overlap(curr_seed_start, curr_seed_range, *source, *amount);
+                    
+                    // modify current seed
+                    current_seeds[i] = (dest + converted.0 - source, converted.1);
 
-        while let Some(seed) = unprocessed.pop_front() {
+                    if front.1 != 0 {
+                        current_seeds.push(front);
+                    }
+                    if back.1 != 0 {
+                        current_seeds.push(back);
+                    }
+                    break;
+                }
+            }
 
-            
+            i += 1;
         }
     }
-    
-    // println!("Min found: {}", min(starting_seeds.iter()).unwrap());
+
+    println!("Min found: {}", min(current_seeds.iter().map(|(a, b)| a)).unwrap());
 }
